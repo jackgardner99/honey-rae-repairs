4afca85f8a9d8a08c25ff07ff4ce5c9e0ddb3ef8
Jack Gardner <jtgardner99@gmail.com>
2026-01-09 15:59:05 -0600

    initial commit

--- src/components/Tickets/Ticket.jsx
+++ src/components/Tickets/Ticket.jsx
@@ -1,3 +1,37 @@
-export const Ticket = () => {
+import { useEffect, useState } from "react"
+import { getEmployees } from "../../services/employeeService"
 
+export const Ticket = ({ ticket }) => {
+    const [employees, setEmployees] = useState([])
+    const [assignedEmployee, setAssignedEmployee] = useState({})
+
+    useEffect(() => {
+        getEmployees().then((employeeArray) => {
+            setEmployees(employeeArray)
+        })
+    }, [])
+
+    useEffect(() => {
+        const foundEmployee = employees.find(
+            (employee) => employee.id === ticket.employeeTickets[0]?.employeeId
+        )
+        setAssignedEmployee(foundEmployee)
+    }, [employees, ticket])
+
+    return (
+        <section className="ticket" key={ticket.id}>
+            <header className="ticket-info">#{ticket.id}</header>
+            <div>{ticket.description}</div>
+            <footer>
+                <div>
+                    <div className="ticket-info">Assignee</div>
+                    <div>{assignedEmployee ? assignedEmployee.user?.fullName : "None"}</div>
+                </div>
+                <div>
+                    <div className="ticket-info">Emergency</div>
+                    <div>{ticket.emergency ? "yes" : "no"}</div>
+                </div>
+             </footer>
+        </section>
+                    )
 }
--- src/components/Tickets/TicketList.jsx
+++ src/components/Tickets/TicketList.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { getAllTickets } from "../../services/ticketService"
+import { Ticket } from "./Ticket"
+import { TicketFilterBar } from "./TicketFilterBar"
 
 export const TicketList = () => {
     const [allTickets, setAllTickets] = useState([])
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         getAllTickets().then((ticketsArray) => {
@@ -23,41 +26,20 @@ export const TicketList = () => {
         }
     }, [showEmergencyOnly, allTickets])
 
+    useEffect(() => {
+        const foundTickets = allTickets.filter((ticket) => {
+            ticket.description.toLowerCase().includes(searchTerm.toLowerCase())
+        })
+        setFilteredTickets(foundTickets)
+    }, [searchTerm, allTickets])
+
     return (
         <div className="tickets-container">
             <h2>Tickets</h2>
-            <div>
-                <button
-                className="filter-btn btn-primary"
-                onClick={() => {
-                    setShowEmergencyOnly(true)
-                }}
-                >
-                    Emergency
-                </button>
-                <button
-                className="filter-btn btn-info"
-                onClick={() => {
-                    setShowEmergencyOnly(false)
-                }}
-                >
-                    Show All
-                </button>
-            </div>
+            <TicketFilterBar setShowEmergencyOnly={setShowEmergencyOnly} setSearchTerm={setSearchTerm} />
             <article className="tickets">
                 {filteredTickets.map((ticket) => {
-                    return (
-                        <section className="ticket" key={ticket.id}>
-                            <header className="ticket-info">#{ticket.id}</header>
-                            <div>{ticket.description}</div>
-                            <footer>
-                                <div>
-                                    <div className="ticket-info">Emergency</div>
-                                    <div>{ticket.emergency ? "yes" : "no"}</div>
-                                </div>
-                            </footer>
-                        </section>
-                    )
+                    return (<Ticket ticket={ticket} key={ticket.id} />)
                 })}
             </article>
         </div>
