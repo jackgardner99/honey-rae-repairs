7b24ad8f2c85420138f451ccc442882b66c7cf45
Jack Gardner <jtgardner99@gmail.com>
2026-01-10 18:45:54 -0600

    initial commit

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -1,4 +1,4 @@
 export const getAllTickets = () => {
-    return fetch("http://localhost:3000/serviceTickets")
+    return fetch("http://localhost:3000/serviceTickets?_embed=employeeTickets")
     .then((res) => res.json())
 }
