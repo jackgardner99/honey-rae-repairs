3d511614096dd174c3b3eee157a438725cc0604f
Jack Gardner <jtgardner99@gmail.com>
2026-01-13 13:51:01 -0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import "./css/App.css"
 import { CustomerList } from "./components/customers/CustomerList"
 import { EmployeeList } from "./components/employees/EmployeeList"
 import { Routes, Route } from "react-router-dom"
+import { NavBar } from "./components/nav/NavBar"
+import { Outlet } from "react-router-dom"
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/tickets" element={<TicketList />} />
-      <Route path="/customers" element={<CustomerList />} />
-      <Route path="/employees" element={<EmployeeList />} />
+      <Route path="/"
+       element={<>
+          <NavBar />
+          <Outlet />
+        </>}>
+          <Route path="tickets" element={<TicketList />} />
+          <Route path="customers" element={<CustomerList />} />
+          <Route path="employees" element={<EmployeeList />} />      
+      </Route>
     </Routes>
   )
 }
